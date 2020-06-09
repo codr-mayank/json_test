deb020e56f75ea3f1fec2604e5f6ab0e49c29111
codr-mayank <gamblergamer9@gmail.com>
2020-06-10 00:54:19 +0530

    add template code for json_test app

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:json_start/ui/home.dart';
+
+void main() => runApp(new MaterialApp(home: BizCardj()));
+  
---
+++ lib/ui/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BizCardj extends StatefulWidget {
+  @override
+  _BizCardjState createState() => _BizCardjState();
+}
+
+class _BizCardjState extends State<BizCardj> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text("Json Test"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            Container(
+              height: 130,
+              child: Card(
+                color: Colors.redAccent,
+                elevation: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 9),
+                    Text('HELLO WORLD'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
